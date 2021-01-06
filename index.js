f38fa3060015bbd21336e5debd6afb6d2d1e43ad
JulianRomana <jjulian.romana@gmail.com>
2021-01-06 15:59:57 +0100

    feat(core): add commit script

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer')
+const git = require('simple-git')()
+const currentGitBranch = git.branch(['-a']).then( result => result.current)
+
+const questions = [
+  {
+    name: 'type',
+    type: 'list',
+    choices: ['feat', 'fix', 'refactor', 'test'],
+    messages:'Choose a commit type',
+  },
+  {
+    name: 'scope',
+    type: 'input',
+    messages: 'Enter scope message'
+  },
+  {
+    name: 'summary',
+    type: 'input',
+    messages: 'Enter summary message'
+  }
+]
+
+inquirer
+  .prompt(questions)
+    .then(({ type, scope, summary }) => {
+      git.add('.')
+      git.commit(`${type}(${(scope)}): ${summary}`)
+      git.push(/* ['--set-upstream', 'origin', currentGitBranch] */ ['--force-with-lease'])
+    })
+    .catch(err =>{console.log(err)})
